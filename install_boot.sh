72761db656cf7660f143d97a1ec158cdda6cb904
lww <1119553797@qq.com>
2016-12-11 11:25:10 +0800

    Update

--- install_boot.sh
+++ install_boot.sh
@@ -1,4 +1,4 @@
 sudo apt install grub-pc-bin grub-efi-ia32-bin grub-efi-amd64-bin -y
-sudo grub-install --target i386-pc --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/lww/WINDOWS/ /dev/$USBDISK_DEVICE
-sudo grub-install --target i386-efi --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/lww/WINDOWS/ /dev/$USBDISK_DEVICE
-sudo grub-install --target x86_64-efi --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/lww/WINDOWS/ /dev/$USBDISK_DEVICE
+sudo grub-install --target i386-pc --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/$USER/WINDOWS/ /dev/$USBDISK_DEVICE
+sudo grub-install --target i386-efi --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/$USER/WINDOWS/ /dev/$USBDISK_DEVICE
+sudo grub-install --target x86_64-efi --removable --boot-directory /media/$USER/WINDOWS/boot --efi-directory /media/$USER/WINDOWS/ /dev/$USBDISK_DEVICE
